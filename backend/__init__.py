b503df3fbcf91dbab26f213fcfc95f34231622aa
jawadsrour20 <67959735+jawadsrour20@users.noreply.github.com>
2022-03-10 18:05:04 +0200

    added requirements.txt

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,3 +1,3 @@
 # The file __init__.py is just an empty file,
-# but it tells Python that <CONTAINING_FOLDER>
+# but it tells Python that the <CONTAINING_FOLDER> ... backend directory in this case
 # with all its modules (Python files) is a package.
